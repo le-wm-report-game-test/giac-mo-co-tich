3789e57c6be5be66e618efd28fe79a70cb3b5086
duycfvn9@gmail.com <duycfvn9@gmail.com>
2026-06-29 09:16:19 +0700

    feat: add Quit_Button image and fix path for quit button on open screen

--- src/ui/main_menu.gd
+++ src/ui/main_menu.gd
@@ -8,7 +8,7 @@ const BG_PATH: String = "res://Assets/OpenScreenAssets/Background_Screen.png"
 const BTN_START_PATH: String = "res://Assets/OpenScreenAssets/Start_Button.png"
 const BTN_CONTINUE_PATH: String = "res://Assets/OpenScreenAssets/Continue_Button.png"
 const BTN_SETTINGS_PATH: String = "res://Assets/OpenScreenAssets/Setting_Button.png"
-const BTN_QUIT_PATH: String = "res://assets/btn_quit.png"
+const BTN_QUIT_PATH: String = "res://Assets/OpenScreenAssets/Quit_Button.png"
 
 # Âm thanh khi hover chuột qua các nút
 @export var hover_sfx: AudioStream = preload("res://Assets/audio/sound_attack.mp3")
